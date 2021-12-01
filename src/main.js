1d4fe15e031c60043ec238c951b5714862d567a9
Michael Bauer <bauermi86944@th-nuernberg.de>
2021-12-02 00:15:15 +0100

    Prepare Pinia state handling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+//#region: PrimeVue and Co.
 import PrimeVue from 'primevue/config';
 import 'primeflex/primeflex.css'
 import 'primevue/resources/themes/lara-dark-teal/theme.css'  //Theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
+//#endregion.
 
-createApp(App).use(PrimeVue).mount("#app");
+
+//#region Pinia store
+import { createPinia, defineStore } from 'pinia'
+
+export const store = defineStore('store', {
+    state: () => ({
+        demandParams: {
+            mean: 30,
+            std_dev: 2,
+            alpha: 0.2
+        }
+    }),
+    actions: {
+        setDemandParams(demandParams) {
+            this.demandParams = demandParams
+        },
+        setDemandParamMean(mean) {
+            this.demandParams.mean = mean
+        },
+        setDemandParamStdDev(std_dev) {
+            this.demandParams.std_dev = std_dev
+        },
+        setDemandParamAlpha(alpha) {
+            this.demandParams.alpha = alpha
+        }
+    }
+})
+//#endregion.
+
+createApp(App).use(createPinia()).use(PrimeVue).mount("#app");
